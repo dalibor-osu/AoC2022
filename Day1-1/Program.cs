b7d98ad6675b02bae842c48cbc7e2d03c6ed751f
Dalibor Dřevojánek <drevojanekdalibor@gmail.com>
2022-12-02 14:37:48 +0100

    Day 1

---
+++ Day1-1/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] input = File.ReadAllLines("./input.txt");
+
+int calories = 0;
+int currentCalories = 0;
+
+foreach (string line in input) {
+    if (line == "") {
+        if(currentCalories > calories) calories = currentCalories;
+        currentCalories = 0;
+    } else {
+        int lineCalories = int.Parse(line);
+        currentCalories += lineCalories;
+    }
+}
+
+Console.WriteLine("Max calories: " + calories);
---
+++ Day1-2/Program.cs
@@ -0,0 +1,31 @@
+﻿string[] input = File.ReadAllLines("./input.txt");
+
+int[] calories = {0, 0, 0};
+int currentCalories = 0;
+
+foreach (string line in input) {
+    if (line == "") {
+        for (int i = 0; i < 3; i++)
+        {
+            if (currentCalories <= calories[i]) continue;
+            
+            if (i == 0) {
+                calories[i + 1] = calories[i];
+                calories[i + 2] = calories[i + 1];
+            }
+            
+            if (i == 1) {
+                calories[i + 1] = calories[i];
+            }
+            
+            calories[i] = currentCalories;
+            break;
+        }
+        
+        currentCalories = 0;
+    } else {
+        currentCalories += int.Parse(line);
+    }
+}
+
+Console.WriteLine("Max calories: " + (calories[0] + calories[1] + calories[2]));
