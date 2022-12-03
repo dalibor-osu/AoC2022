f0964e3bff48791b799d0e549055fdd3ef3f59ad
Dalibor Dřevojánek <drevojanekdalibor@gmail.com>
2022-12-03 13:17:13 +0100

    Day 3

---
+++ Day3-1/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] input = File.ReadAllLines("./input.txt");
+string types = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int points = 0;
+
+foreach (string line in input) {
+    string first = line.Substring(0, line.Length / 2);
+    string second = line.Substring(line.Length / 2, line.Length / 2);
+    char commonType = ' ';
+
+    foreach (var c in first) {
+        if (second.Contains(c))
+        {
+            commonType = c;
+            break;
+        }
+    }
+
+    points += types.IndexOf(commonType) + 1;
+}
+
+Console.WriteLine("Total points: " + points);
---
+++ Day3-2/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] input = File.ReadAllLines("./input.txt");
+string types = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int points = 0;
+
+for (int i = 0; i < input.Length; i += 3) {
+    char commonType = ' ';
+    
+    foreach (char c in input[i]) {
+        if (input[i + 1].Contains(c) && input[i + 2].Contains(c)) {
+            commonType = c;
+            break;
+        }
+    }
+    
+    points += types.IndexOf(commonType) + 1;
+}
+
+Console.WriteLine("Total points: " + points);
