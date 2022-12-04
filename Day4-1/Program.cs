4b6175c99a911dc08811ff2b84f7847e20c96503
Dalibor Dřevojánek <drevojanekdalibor@gmail.com>
2022-12-04 20:09:41 +0100

    Day 4

---
+++ Day4-1/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] input = File.ReadAllLines("./input.txt");
+int fullyContained = 0;
+
+foreach (string line in input)
+{
+    string[] sections = line.Split(',');
+    string[] firstSection = sections[0].Split('-');
+    string[] secondSection = sections[1].Split('-');
+    
+    int firstMin = int.Parse(firstSection[0]);
+    int firstMax = int.Parse(firstSection[1]);
+    int secondMin = int.Parse(secondSection[0]);
+    int secondMax = int.Parse(secondSection[1]);
+
+    if (firstMin <= secondMin && firstMax >= secondMax) {
+        fullyContained += 1;
+        continue;
+    }
+
+    if (secondMin <= firstMin && secondMax >= firstMax) {
+        fullyContained += 1;
+    }
+}
+
+Console.WriteLine(fullyContained);
